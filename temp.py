94f5141084144260b83cb33c6ad744ed60ec7530
SyedUsman.AliBukhari <usman40e@gmail.com>
2024-04-09 15:14:11 +0500

    Ok

--- temp.py
+++ temp.py
@@ -1,69 +1,79 @@
-import os, glob
-import pandas as pd
-import psycopg2
+# import os, glob
+# import pandas as pd
+# import psycopg2
 
-def squeeze(x):
-    return x['0']
+# def squeeze(x):
+#     return x['0']
 
-if __name__ == '__main__':
-    root_dir = r'D:\ITU\Semester 2\Big Data\Assignments\Assignment 2\Assignment 2\data\youtube_data'
-    json_files = []
-    for root, dirs, files in os.walk(root_dir):
-        for file in files:
-            if file.endswith(".json"):
-                json_files.append(os.path.join(root, file))
+# if __name__ == '__main__':
+#     root_dir = r'D:\ITU\Semester 2\Big Data\Assignments\Assignment 2\Assignment 2\data\youtube_data'
+#     json_files = []
+#     for root, dirs, files in os.walk(root_dir):
+#         for file in files:
+#             if file.endswith(".json"):
+#                 json_files.append(os.path.join(root, file))
 
-    cols = ['youtuber_id', 'youtuber_name', 'youtuber_location', 'youtuber_latitude', 'youtuber_longitude']
+#     cols = ['youtuber_id', 'youtuber_name', 'youtuber_location', 'youtuber_latitude', 'youtuber_longitude']
 
-    df = pd.read_json(json_files[0], lines=True)
+#     df = pd.read_json(json_files[0], lines=True)
 
-    rows = []
-    for i in range(len(json_files)):
-        df = pd.read_json(json_files[i], lines=True)
-        row = df.iloc[0][cols].apply(squeeze)
-        row.fillna(0, inplace=True)
-        rows.append(tuple(row.values))
+#     rows = []
+#     for i in range(len(json_files)):
+#         df = pd.read_json(json_files[i], lines=True)
+#         row = df.iloc[0][cols].apply(squeeze)
+#         row.fillna(0, inplace=True)
+#         rows.append(tuple(row.values))
     
-    print(rows[0])
 
-    # cols = ['video_id', 'video_id', 'youtuber_id', 'year', 'duration']
+#     cols = ['video_id', 'title', 'year', 'duration']
     
-    # rows = []
-    # for i in range(len(json_files)):
-    #     df = pd.read_json(json_files[i], lines=True)
-    #     row = df.iloc[0][cols].apply(squeeze)
-    #     row.fillna(0, inplace=True)
-    #     rows.append(tuple(row.values))
+#     rows = []
+#     for i in range(len(json_files)):
+#         df = pd.read_json(json_files[i], lines=True)
+#         row = df.iloc[0][cols].apply(squeeze)
+#         row.fillna(0, inplace=True)
+#         rows.append(tuple(row.values))
 
 
 
-    # Establish a connection to your PostgreSQL database
-    conn = psycopg2.connect(
-        dbname="youtubedb",
-        user="postgres",
-        password="paybox",
-        host="localhost",
-        port="5432"
-    )
+#     # Establish a connection to your PostgreSQL database
+#     conn = psycopg2.connect(
+#         dbname="youtubedb",
+#         user="postgres",
+#         password="paybox",
+#         host="localhost",
+#         port="5432"
+#     )
 
-    # Create a cursor object
-    cur = conn.cursor()
+#     # Create a cursor object
+#     cur = conn.cursor()
 
 
-    # Define your insert statement string
-    insert_statement = """
-        INSERT INTO YOUTUBERS_DIM
-        (YOUTUBER_ID, NAME, LOCATION, LATITUDE, LONGITUDE)
-        VALUES
-        (%s, %s, %s, %s, %s)
-    """
+#     # Define your insert statement string
+#     insert_statement = """
+#             INSERT INTO VIDEOS_DIM
+#             (VIDEO_ID, TITLE, YEAR, DURATION)
+#             VALUES
+#             (%s, %s, %s, %s)
+#     """
 
-    # Execute the insert statement for each tuple in the data list
-    cur.executemany(insert_statement, rows)
+#     # Execute the insert statement for each tuple in the data list
+#     cur.executemany(insert_statement, rows)
 
-    # Commit the transaction
-    conn.commit()
+#     # Commit the transaction
+#     conn.commit()
 
-    # Close the cursor and connection
-    cur.close()
-    conn.close()
+#     # Close the cursor and connection
+#     cur.close()
+#     conn.close()
+
+
+Youtubers_table_insert = ("""
+INSERT INTO YOUTUBERS_DIM (YOUTUBER_ID, NAME, LOCATION, LATITUDE, LONGITUDE)
+VALUES ('%s', '%s', '%s', %d, %d)
+""")
+
+data =  ('ARD7TVE1187B99BFB1', 'Casual', 'California - LA', 0, 0)
+
+
+print(Youtubers_table_insert % data)
